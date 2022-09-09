7b2e7647cc11a030df4b3c6c052deb3981f887d9
Rio Edwards <rioredwards@gmail.com>
2022-09-09 11:55:11 -0700

    Create Plan

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-// import functions and grab DOM elements
-
-// let state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// inputContainer.classList.toggle('hide');
